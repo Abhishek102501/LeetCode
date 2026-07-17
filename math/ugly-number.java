7f6ae2e9ef8f401ac0334066c9e1964e38b64df4
Abhishek Dubey <abhi.shek.897984@gmail.com>
2026-07-17 13:21:14 +0530

    LC-ugly-number [Runtime: N/A; Memory: N/A]

--- math/ugly-number.java
+++ math/ugly-number.java
@@ -1,11 +1,16 @@
 class Solution {
     public boolean isUgly(int n) {
-        if(n % 1 == 0){
-            return true;
-        }
-        if((n % 2 == 0) || (n % 3 == 0) || (n % 5 == 0)){
-            return true;
-        }
+       if(n < 0){
         return false;
-    }
+       }
+
+       int[] factors = {2, 3, 5};
+
+       for(int factor : factors){
+        while(n % factor == 0){
+            n /= factor; 
+        }
+       }
+       return n == 1;
+       }
 }
